e5e15df898642f257078101cafc055b89726dc95
alexsie48 <alexsie48@gmail.com>
2018-11-24 15:04:47 -0500

    Added the main menu tree.

--- Main.m
+++ Main.m
@@ -1,2 +1,16 @@
 %% Main File
 %% this is where most of the code will go
+
+
+Yes = 1
+No = 0
+
+fprintf('Welcome to yatzee\n')
+fprintf('would you like to play?\n')
+Menu = input('Yes or No? \n'); 
+
+if Menu == 0
+    fprintf ('Have a great day.')
+else
+    fprintf('Okay then lets start the game!')
+end

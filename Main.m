cc246912393bb4719e22998cd45b3d2d3efc8136
alexsie48 <alexsie48@gmail.com>
2018-11-25 00:00:01 -0500

    added the first rerole option to program

--- Main.m
+++ Main.m
@@ -25,9 +25,45 @@ role = randi([1 6], [1 5])
 %%this dispays the role using the Dice.mat file
 imshow([Dice{role}]);
 
-fprintf('Would you like to rerole any dice?\n')
+reference = role 
 
-redo = input('list the dice which you would like to rerole:   ')
+rerole = input('Would you like to rerole any dice?\n')
+
+%%redo = input('list the dice which you would like to rerole:   ')
+
+if rerole == 0
+    fprintf('Congrats this is your score')
+    quit
+else
+    NR1 = input('Would you like to rerole dice 1?  ');
+    NR2 = input('Would you like to rerole dice 2?  ');
+    NR3 = input('Would you like to rerole dice 3?  ');
+    NR4 = input('Would you like to rerole dice 4?  ');
+    NR5 = input('Would you like to rerole dice 5?  ');
+end
+
+role2 = randi([1 6], [1 5])
+
+if NR1 == 1
+    reference(1) = role2(1);
+end
+
+if NR2 == 1
+    reference(2) = role2(2);
+end
+
+if NR3 == 1
+    reference(3) = role2(3);
+end
+
+if NR4 == 1
+    reference(4) = role2(4);
+end
+
+if NR5 == 1
+    reference(5) = role2(5);
+end
 
 
-
+imshow([Dice{reference}]);
+    

cec55981ee949f97fb0c2bac85a364e123bef68d
alice.5 <alice.5@MRN187315.bcd.it.osu.edu>
2018-11-26 10:23:27 -0500

    added comments

--- Main.m
+++ Main.m
@@ -1,40 +1,34 @@
-%% Main File
-    %%This is where most of the code will go
+%% Main File for Yahtzee 
+%loading the dice
 load Dice.mat
 
 Yes = 1;
 No = 0;
 
 
-%% This is the main menu Prompt
+%% This is the main menu 
 
-fprintf('Welcome to yatzee\n')
-fprintf('would you like to play?\n')
+fprintf('Welcome to Yahtzee\n')
+fprintf('Would you like to play?\n')
 Menu = input('Yes or No? \n'); 
-
+% User confirmation to start program 
 if Menu == 0
     fprintf ('Have a great day.')
     quit
 else
     fprintf('Okay then lets start the game!\n')
 end
-    %%just a check for user to confirm start of program
 
 %% This is the first Role
-
+% Roles dice randomly 
 role = randi([1 6], [1 5]);
-    %%roles dice using random numbers
-
+% displays dice
 imshow([Dice{role}]);
-    %%this dispays the role using the Dice.mat file
-
 
 %% This is the start of the first ReRole
-
+% used for troubleshooting issues in rerole function
 reference = role ;
-    %%used for troubleshooting issues in ReRole function
-    %%a copy of the original role so i can reference back to the original
-
+% asking user what dice they would like to rerole
 rerole = input('Would you like to rerole any dice?\n');
 
 if rerole == 1
@@ -44,12 +38,9 @@ if rerole == 1
     NR3 = input('Would you like to rerole dice 3?  ');
     NR4 = input('Would you like to rerole dice 4?  ');
     NR5 = input('Would you like to rerole dice 5?  ');
-        %%used for determining what dice need to be rerolled at the request
-        %%of the user
-
+    % a random array for later use 
     role2 = randi([1 6], [1 5]);
-        %%a new random number aray for later use
-
+    % which dice to replace and what numbers to replace them with 
     if NR1 == 1
         reference(1) = role2(1);
     end
@@ -69,28 +60,22 @@ if rerole == 1
     if NR5 == 1
         reference(5) = role2(5);
     end
-        %%based on the users selection these conditional statements replace
-        %%the original dice roles with the new coresponding dice values
-
+    % displays new roll  
     imshow([Dice{reference}]);
-        %%displays the new dice roll
 
 %% This is the start of the second ReRole
-
+% asking user if they would like to rerole
     rerole = input('Would you like to rerole any dice?\n');
-
+%asking user what dice they would like to rerole
     if rerole == 1
         NR1 = input('Would you like to rerole dice 1?  ');
         NR2 = input('Would you like to rerole dice 2?  ');
         NR3 = input('Would you like to rerole dice 3?  ');
         NR4 = input('Would you like to rerole dice 4?  ');
         NR5 = input('Would you like to rerole dice 5?  ');
-            %%used for determining what dice need to be rerolled at the request
-            %%of the user
-        
+        % a random array for later use 
         role3 = randi([1 6], [1 5]);
-            %%a new random number aray for later use
-        
+        % which dice to replace and what numbers to replace them with 
         if NR1 == 1
             reference(1) = role3(1);
         end
@@ -110,11 +95,9 @@ if rerole == 1
         if NR5 == 1
             reference(5) = role3(5);
         end
-            %%based on the users selection these conditional statements replace
-            %%the original dice roles with the new coresponding dice values
-
+        %displays the new dice roll
         imshow([Dice{reference}]);
-            %%displays the new dice roll
+        
             
         fprintf('Time to select your catagory! \n')
     else
@@ -131,22 +114,22 @@ total = sum(reference);
 up = [0 0 0 0 0 0];
 
 fprintf('You must first select a catagory you would like to score in the upper section. \n')
-
+%counts the number of each dice in its catagory
 up(1) = sum(reference(:) == 1);
 up(2) = sum(reference(:) == 2);
 up(3) = sum(reference(:) == 3);
 up(4) = sum(reference(:) == 4);
 up(5) = sum(reference(:) == 5);
 up(6) = sum(reference(:) == 6);
-    %%counts the number of each dice in its catagory
 
+%for corectly scorring each catagory 
 Aces = up(1) * 1;
 Twos = up(2) * 2;
 Threes = up(3) * 3;
 Fours = up(4) * 4;
 Fives = up(5) * 5;
 Sixes = up(6) * 6;
-    %%for corectly scorring each catagory 
+
 
 upScore = input('You may choose between Aces, Twos, Threes, Fours, Fives, Sixes:    ');
 
@@ -168,32 +151,30 @@ lowScore = 0;
 %    end
 %end
     %%This loop is currently not working. comment out if a run is required.
-    
+% if you get a yahtzee   
 if any(up == 5)
     lowScore = 50;
     fprintf('Wow you got a Yahtzee')
 end
-
+% four of a kind 
 if any(up == 4)
     lowScore = total;
     fprintf('Wow you got a four of a kind!')
 end
-
+% full house 
 if any(up ==3 & up == 2)
     lowScore = 25;
     fprintf('Wow you got a full house!')
 end
-
+% three of a kind 
 if any(up == 3)
     lowScore = total;
     fprintf('Wow you got a Three of a kind!')
 end
-
+% large straight
 if any( up > 1)
     lowScore = 40;
     fprintf('Wow you got a Large Straight')
 end
-
-
+% end score
 gameScore = upScore + lowScore;
-%%not a hunred percent sure if this how yahtzee works lol
